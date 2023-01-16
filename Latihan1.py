9caee1ed0afb827794f4682545f917ae591b68f9
arie_miftah <arie.miftah3@gmail.com>
2023-01-16 18:19:36 +0700

    Folder

---
+++ Latihan1.py
@@ -0,0 +1,23 @@
+txt = 'Hello World'
+print ("Jumlah karakternya adalah : ", len (txt))
+print("<=============================>")
+txt = 'Hello World'
+print (txt[6:12])
+print("<===============>")
+txt = 'Hello World'
+print (txt[2:5])
+print("<===============>")
+txt = 'Hello World'
+x = txt.replace(" ","")
+print (x)
+print("<===============>")
+txt = 'Hello World'
+print (txt.upper())
+print("<===============>")
+txt = 'Hello World'
+print (txt.lower())
+print("<===============>")
+txt = 'Hello World'
+new_txt = 'J' + txt[1:]
+print (new_txt)
+print("<===============>")
---
+++ Latihan2.py
@@ -0,0 +1,3 @@
+umur = 24
+txt = "Hello, nama saya john, dan umur saya adalah %d tahun" % (24)
+print(txt.format(umur))
